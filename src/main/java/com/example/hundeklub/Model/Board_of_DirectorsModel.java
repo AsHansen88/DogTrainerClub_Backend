dba875dea51c3e4435577e2f8a02e010ecd161ed
AsHansen88 <Ande970s@stud.kea.dk>
2023-05-01 13:23:59 +0200

    første push

--- src/main/java/com/example/hundeklub/Model/Board_of_DirectorsModel.java
+++ src/main/java/com/example/hundeklub/Model/Board_of_DirectorsModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="fileTable")
+@Table(name="dogtrainer")
 public class Board_of_DirectorsModel {
 
 
--- src/main/java/com/example/hundeklub/Repository/TraningTeamRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.hundeklub.Repository;
-
-public interface TraningTeamRepository {
-}
